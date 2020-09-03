10d262d4cf104a4f83a1d217b1f34dddf541f282
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-09-03 17:29:01 +0900

    feat: answer problem-1

---
+++ problem-1/src/main.rs
@@ -0,0 +1,13 @@
+// http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%201
+
+// 10未満の自然数のうち, 3 もしくは 5 の倍数になっているものは 3, 5, 6, 9 の4つがあり, これらの合計は 23 になる.
+// 同じようにして, 1000 未満の 3 か 5 の倍数になっている数字の合計を求めよ.
+
+fn main() {
+    println!(
+        "{:?}",
+        (1..1000)
+            .filter(|n| { n % 3 == 0 || n % 5 == 0 })
+            .sum::<usize>()
+    )
+}
