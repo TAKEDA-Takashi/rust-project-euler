983fb7b5d5d050f2318ae1757120bf2d32f98d44
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-09-15 17:20:11 +0900

    feat: answer problem-010

---
+++ problem-010/src/main.rs
@@ -0,0 +1,13 @@
+//! http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2010
+//!
+//! 10以下の素数の和は 2 + 3 + 5 + 7 = 17 である.
+//! 200万以下の全ての素数の和を求めよ.
+
+use problem_010::Prime;
+
+fn main() {
+    println!(
+        "{}",
+        Prime::new().take_while(|p| *p < 2_000_000).sum::<usize>()
+    );
+}
