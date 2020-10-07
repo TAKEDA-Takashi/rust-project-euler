fcf237c0a3cae4946b247090c8875cb36eb757be
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-10-07 18:59:03 +0900

    feat: answer problem-049

---
+++ problem-049/src/main.rs
@@ -0,0 +1,41 @@
+//! http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2049
+//!
+//! 項差3330の等差数列1487, 4817, 8147は次の2つの変わった性質を持つ.
+//!
+//! (i)3つの項はそれぞれ素数である.
+//! (ii)各項は他の項の置換で表される.
+//! 1, 2, 3桁の素数にはこのような性質を持った数列は存在しないが, 4桁の増加列にはもう1つ存在する.
+//!
+//! それではこの数列の3つの項を連結した12桁の数を求めよ.
+
+use itertools::Itertools;
+use problem_049::Prime;
+use std::collections::{BTreeSet, HashSet};
+
+fn main() {
+    let set: BTreeSet<usize> = Prime::new()
+        .skip_while(|&p| p < 1000)
+        .take_while(|&p| p < 10000)
+        .collect();
+
+    println!(
+        "{}",
+        set.iter()
+            .combinations(2)
+            .map(|c| [*c[0], *c[1], c[1] + c[1] - c[0]])
+            .filter(|[.., c]| set.contains(c))
+            .filter(|[a, b, c]| {
+                let (x, y, z) = (
+                    a.to_string().chars().collect::<HashSet<_>>(),
+                    b.to_string().chars().collect::<HashSet<_>>(),
+                    c.to_string().chars().collect::<HashSet<_>>(),
+                );
+
+                x == y && y == z
+            })
+            .find(|&[a, ..]| a != 1487)
+            .unwrap()
+            .iter()
+            .join("")
+    );
+}
