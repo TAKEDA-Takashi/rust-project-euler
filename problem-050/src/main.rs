2d1e63ffcd7bf01cf43c1b2f7d3c091ccf003adf
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-10-08 08:51:37 +0900

    feat: answer problem-050

---
+++ problem-050/src/main.rs
@@ -0,0 +1,47 @@
+//! http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2050
+//!
+//! 素数41は6つの連続する素数の和として表せる:
+//!
+//! 41 = 2 + 3 + 5 + 7 + 11 + 13.
+//! 100未満の素数を連続する素数の和で表したときにこれが最長になる.
+//!
+//! 同様に, 連続する素数の和で1000未満の素数を表したときに最長になるのは953で21項を持つ.
+//!
+//! 100万未満の素数を連続する素数の和で表したときに最長になるのはどの素数か?
+
+use problem_050::Prime;
+use std::collections::HashSet;
+
+fn main() {
+    let upper_bound = 1_000_000;
+    let prime_list: Vec<usize> = Prime::new().take_while(|&p| p < upper_bound).collect();
+    let prime_set: HashSet<&usize> = prime_list.iter().collect();
+
+    let (.., max_prime, _max_len) = (0..prime_list.len())
+        .map(|i| {
+            prime_list[i..]
+                .iter()
+                .try_fold(
+                    /* sum, len, prime, max_len */ (0, 0, 0, 0),
+                    |acc, &n| {
+                        if acc.0 >= upper_bound {
+                            Err(acc)
+                        } else {
+                            let sum = acc.0 + n;
+                            let len = acc.1 + 1;
+
+                            if prime_set.contains(&sum) {
+                                Ok((sum, len, sum, len))
+                            } else {
+                                Ok((sum, len, acc.2, acc.3))
+                            }
+                        }
+                    },
+                )
+                .unwrap_or_else(|n| n)
+        })
+        .max_by(|(.., len1), (.., len2)| len1.cmp(len2))
+        .unwrap();
+
+    println!("{}", max_prime);
+}
