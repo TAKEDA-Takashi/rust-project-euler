02d9fdc528aeb89f9e65f42a3686d18f5280251a
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-10-08 22:04:10 +0900

    feat: answer problem-053

---
+++ problem-053/src/main.rs
@@ -0,0 +1,32 @@
+//! http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2053
+//!
+//! 12345から3つ選ぶ選び方は10通りである.
+//!
+//! 123, 124, 125, 134, 135, 145, 234, 235, 245, 345.
+//! 組み合わせでは, 以下の記法を用いてこのことを表す: 5C3 = 10.
+//!
+//! 一般に, r ≤ n について nCr = n!/(r!(n-r)!) である. ここで, n! = n×(n−1)×...×3×2×1, 0! = 1 と階乗を定義する.
+//!
+//! n = 23 になるまで, これらの値が100万を超えることはない: 23C10 = 1144066.
+//!
+//! 1 ≤ n ≤ 100 について, 100万を超える nCr は何通りあるか?
+
+use std::iter::repeat;
+
+fn main() {
+    println!(
+        "{}",
+        (1..=100)
+            .flat_map(|n| repeat(n).zip(0..=n))
+            .filter(|&(n, r)| combination(n, r) > 1_000_000)
+            .count()
+    );
+}
+
+fn combination(n: u128, r: u128) -> u128 {
+    if r == 0 {
+        return 1;
+    }
+
+    combination(n, r - 1) * (n - r + 1) / r
+}
