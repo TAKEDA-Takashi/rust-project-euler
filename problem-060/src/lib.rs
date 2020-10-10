4116a438ff854e02ffcf660cb2de37153b78231f
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-10-10 20:40:26 +0900

    feat: answer problem-060

---
+++ problem-060/src/lib.rs
@@ -0,0 +1,107 @@
+// Same problem-041
+
+use rand::random;
+
+const MILLER_RABIN_ROUND: usize = 20;
+
+pub fn is_prime(n: usize) -> bool {
+    if n == 2 {
+        true
+    } else if n < 2 || n % 2 == 0 {
+        false
+    } else {
+        miller_rabin_test(n)
+    }
+}
+
+fn miller_rabin_test(n: usize) -> bool {
+    let (d, s) = find_odd(n - 1);
+
+    (0..MILLER_RABIN_ROUND).all(|_| {
+        let a = (random::<f64>() * ((n - 3) as f64)) as usize + 2;
+        let y = modpow(a, d, n);
+        if y == 1 {
+            true
+        } else {
+            (0..s).any(|r| modpow(y, 2_usize.pow(r as u32), n) == n - 1)
+        }
+    })
+}
+
+fn find_odd(q: usize) -> (usize, usize) {
+    fn find_odd0(q: usize, s: usize) -> (usize, usize) {
+        if q & 1 == 1 {
+            (q, s)
+        } else {
+            find_odd0(q >> 1, s + 1)
+        }
+    }
+
+    find_odd0(q, 0)
+}
+
+fn modpow(b: usize, e: usize, m: usize) -> usize {
+    fn modpow0(b: usize, e: usize, m: usize, r: usize) -> usize {
+        if e == 0 {
+            r
+        } else {
+            modpow0(b * b % m, e >> 1, m, if e & 1 == 1 { r * b % m } else { r })
+        }
+    }
+
+    modpow0(b, e, m, 1)
+}
+
+// Same problem-007
+
+#[derive(Debug)]
+pub struct Prime {
+    ps: Vec<usize>,
+}
+
+impl Prime {
+    /// 素数列を生成するイテレータを生成する。
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use problem_060::Prime;
+    ///
+    /// let mut prime = Prime::new();
+    /// assert_eq!(prime.next(), Some(2));
+    /// assert_eq!(prime.next(), Some(3));
+    /// assert_eq!(prime.next(), Some(5));
+    /// assert_eq!(prime.next(), Some(7));
+    /// ```
+    pub fn new() -> Prime {
+        Prime { ps: vec![] }
+    }
+}
+
+impl Iterator for Prime {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.ps.last() {
+            None => {
+                self.ps.push(2);
+                Some(2)
+            }
+            Some(2) => {
+                self.ps.push(3);
+                Some(3)
+            }
+            Some(last_prime) => {
+                let next_prime = ((last_prime + 2)..).step_by(2).find(|&n| {
+                    self.ps
+                        .iter()
+                        .take_while(|&&p| p * p <= n)
+                        .all(|&p| n % p != 0)
+                })?;
+
+                self.ps.push(next_prime);
+                Some(next_prime)
+            }
+        }
+    }
+}
---
+++ problem-060/src/main.rs
@@ -0,0 +1,48 @@
+//! http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2060
+//!
+//! 素数3, 7, 109, 673は非凡な性質を持っている. 任意の2つの素数を任意の順で繋げると, また素数になっている. 例えば, 7と109を用いると, 7109と1097の両方が素数である. これら4つの素数の和は792である. これは, このような性質をもつ4つの素数の集合の和の中で最小である.
+//!
+//! 任意の2つの素数を繋げたときに別の素数が生成される, 5つの素数の集合の和の中で最小のものを求めよ.
+
+use itertools::Itertools;
+use problem_060::{is_prime, Prime};
+use std::iter::{once, repeat};
+
+fn main() {
+    let upper_bound = 10_000;
+
+    let prime_list = Prime::new().take_while(|&p| p < upper_bound).collect_vec();
+    let v = find_prime_pair_sets(&prime_list, 5).unwrap();
+
+    println!("{}", v.iter().sum::<usize>());
+}
+
+fn find_prime_pair_sets(prime_list: &Vec<usize>, find_len: usize) -> Option<Vec<usize>> {
+    fn find0(prime_list: &Vec<usize>, find_len: usize, v: Vec<usize>) -> Option<Vec<usize>> {
+        if v.len() == find_len {
+            Some(v)
+        } else {
+            let maximum = *v.last().unwrap();
+            repeat(v)
+                .zip(prime_list.iter().skip_while(|&&p| p <= maximum))
+                .map(|(v, n)| v.into_iter().chain(once(*n)).collect_vec())
+                .filter(is_prime_pair)
+                .filter_map(|v| find0(prime_list, find_len, v))
+                .next()
+        }
+    }
+
+    prime_list
+        .iter()
+        .filter_map(|&p| find0(prime_list, find_len, vec![p]))
+        .next()
+}
+
+fn is_prime_pair(v: &Vec<usize>) -> bool {
+    let p = *v.last().unwrap();
+
+    (0..v.len() - 1).all(|i| {
+        is_prime(v[i] * 10_usize.pow((p as f64).log10() as u32 + 1) + p)
+            && is_prime(p * 10_usize.pow((v[i] as f64).log10() as u32 + 1) + v[i])
+    })
+}
