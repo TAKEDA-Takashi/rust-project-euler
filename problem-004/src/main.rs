866c8e65246dc4ff5d05ec7de894f6ae334fc43e
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-10-12 00:12:12 +0900

    refactor: problem-004

--- problem-004/src/main.rs
+++ problem-004/src/main.rs
@@ -2,28 +2,14 @@
 //!
 //! 左右どちらから読んでも同じ値になる数を回文数という. 2桁の数の積で表される回文数のうち, 最大のものは 9009 = 91 × 99 である.
 //! では, 3桁の数の積で表される回文数の最大値を求めよ.
-use std::iter::repeat;
 
+use euler_lib::mul_palindrome;
 use itertools::Itertools;
-
-fn is_palindrome(n: i32) -> bool {
-    let s = n.to_string();
-    s == s.chars().rev().collect::<String>()
-}
-
-fn to_palindrome(t: (i32, i32)) -> Option<(i32, i32, i32)> {
-    let m = t.0 * t.1;
-    if is_palindrome(m) {
-        Some((t.0, t.1, m))
-    } else {
-        None
-    }
-}
 
 fn main() {
     let max_palindrome = (100..=999)
-        .flat_map(|n| repeat(n).zip(n..=999))
-        .filter_map(to_palindrome)
+        .combinations(2)
+        .filter_map(|v| mul_palindrome(&(v[0], v[1])))
         .sorted_by(|(.., p1), (.., p2)| Ord::cmp(p2, p1)) // order by desc
         .next()
         .unwrap();
