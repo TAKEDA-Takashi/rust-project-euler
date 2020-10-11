8bd6e86158e1fdb5adaedef3b50e33ac0edb2bc5
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-10-11 23:30:21 +0900

    refactor: problem-007

--- problem-007/src/main.rs
+++ problem-007/src/main.rs
@@ -3,8 +3,8 @@
 //! 素数を小さい方から6つ並べると 2, 3, 5, 7, 11, 13 であり, 6番目の素数は 13 である.
 //! 10 001 番目の素数を求めよ.
 
-use problem_007::Prime;
+use euler_lib::Prime;
 
 fn main() {
-    println!("{}", Prime::new().nth(10000).unwrap()); // 0-origin
+    println!("{}", Prime::<usize>::new().iter().nth(10000).unwrap()); // 0-origin
 }
