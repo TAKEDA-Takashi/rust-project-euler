4201f07d791225183f00db84d6663c50c1cdd131
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-10-12 00:32:15 +0900

    refactor: problem-010

--- problem-010/src/main.rs
+++ problem-010/src/main.rs
@@ -3,11 +3,14 @@
 //! 10以下の素数の和は 2 + 3 + 5 + 7 = 17 である.
 //! 200万以下の全ての素数の和を求めよ.
 
-use problem_010::Prime;
+use euler_lib::Prime;
 
 fn main() {
     println!(
         "{}",
-        Prime::new().take_while(|&p| p <= 2_000_000).sum::<usize>()
+        Prime::<usize>::new()
+            .iter()
+            .take_while(|&p| p <= 2_000_000)
+            .sum::<usize>()
     );
 }
