65150ecd037f7dfb3086fd4838c84b601b4ec702
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-10-12 00:28:37 +0900

    refactor: problem-005

--- problem-005/src/main.rs
+++ problem-005/src/main.rs
@@ -3,14 +3,8 @@
 //! 2520 は 1 から 10 の数字の全ての整数で割り切れる数字であり, そのような数字の中では最小の値である.
 //! では, 1 から 20 までの整数全てで割り切れる数字の中で最小の正の数はいくらになるか.
 
-fn main() {
-    println!("{}", (2..=20).fold(1, |acc, n| acc * n / gcd(acc, n)));
-}
+use euler_lib::gcd;
 
-fn gcd(a: usize, b: usize) -> usize {
-    if b == 0 {
-        a
-    } else {
-        gcd(b, a % b)
-    }
+fn main() {
+    println!("{}", (2..=20_u64).fold(1, |acc, n| acc * n / gcd(&acc, &n)));
 }
