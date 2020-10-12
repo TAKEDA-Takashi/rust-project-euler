4fa00e660c21cae827096c3b4f1fbd00efe787ad
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-10-12 12:26:59 +0900

    feat: euler-lib impl iter_intersect

--- euler-lib/src/lib.rs
+++ euler-lib/src/lib.rs
@@ -1,4 +1,5 @@
 use num::{one, range_step, zero, CheckedAdd, FromPrimitive, Integer};
+use std::cmp::Ordering;
 use std::collections::BTreeMap;
 
 pub mod func;
@@ -176,6 +177,48 @@ where
     }
 }
 
+pub struct Intersect<T1, T2>
+where
+    T1: Iterator,
+    T2: Iterator,
+{
+    iter1: T1,
+    iter2: T2,
+}
+
+impl<T1, T2> Iterator for Intersect<T1, T2>
+where
+    T1: Iterator,
+    T2: Iterator<Item = T1::Item>,
+    T1::Item: Ord + Copy,
+{
+    type Item = T1::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut t1 = self.iter1.next();
+        let mut t2 = self.iter2.next();
+
+        loop {
+            match t1.zip(t2).map(|(a, b)| a.cmp(&b)) {
+                Some(Ordering::Less) => t1 = self.iter1.next(),
+                Some(Ordering::Greater) => t2 = self.iter2.next(),
+                Some(Ordering::Equal) => break,
+                None => return None,
+            }
+        }
+
+        t1
+    }
+}
+
+pub fn iter_intersect<T1, T2>(iter1: T1, iter2: T2) -> Intersect<T1, T2>
+where
+    T1: Iterator,
+    T2: Iterator,
+{
+    Intersect { iter1, iter2 }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -214,5 +257,18 @@ mod tests {
         assert_eq!(Some(3), fibo.next());
         assert_eq!(Some(5), fibo.next());
         assert_eq!(Some(8), fibo.next());
+    }
+
+    #[test]
+    fn intersect() {
+        let mut intersect = iter_intersect(
+            vec![1, 3, 5, 9, 11, 13].into_iter(),
+            vec![1, 2, 3, 5, 8, 13].into_iter(),
+        );
+        assert_eq!(intersect.next(), Some(1));
+        assert_eq!(intersect.next(), Some(3));
+        assert_eq!(intersect.next(), Some(5));
+        assert_eq!(intersect.next(), Some(13));
+        assert_eq!(intersect.next(), None);
     }
 }
