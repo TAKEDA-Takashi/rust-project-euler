5f66b303f684f13215a74ab59271bbfad754280d
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-10-17 17:39:11 +0900

    feat: answer problem-076

---
+++ problem-076/src/main.rs
@@ -0,0 +1,30 @@
+//! http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2076
+//!
+//! 5は数の和として6通りに書くことができる:
+//!
+//! 4 + 1
+//! 3 + 2
+//! 3 + 1 + 1
+//! 2 + 2 + 1
+//! 2 + 1 + 1 + 1
+//! 1 + 1 + 1 + 1 + 1
+//!
+//! 2つ以上の正整数の和としての100の表し方は何通りか.
+
+fn main() {
+    println!("{}", counting_summations(&100));
+}
+
+fn counting_summations(n: &usize) -> usize {
+    fn counting_summation0(n: usize, head: usize) -> usize {
+        if head == 1 || n < 2 {
+            1
+        } else if n < head {
+            counting_summation0(n, n)
+        } else {
+            counting_summation0(n, head - 1) + counting_summation0(n - head, head)
+        }
+    }
+
+    counting_summation0(*n, n - 1)
+}
