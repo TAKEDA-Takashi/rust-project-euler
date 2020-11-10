8255de23d56369e91269cf8aac92c2bb8fe5baaf
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-11-10 16:09:34 +0900

    refactor: problem-048

--- project-euler/src/bin/problem-048.rs
+++ project-euler/src/bin/problem-048.rs
@@ -4,21 +4,11 @@
 //!
 //! では, 11 + 22 + 33 + ... + 10001000 の最後の10桁を求めよ.
 
+use euler_lib::modpow;
+
 fn main() {
     println!(
         "{}",
         (1..1000).map(|n| modpow(n, n, 10000000000)).sum::<u128>() % 10000000000
     );
-}
-
-fn modpow(b: u128, e: u128, m: u128) -> u128 {
-    modpow0(b, e, m, 1)
-}
-
-fn modpow0(b: u128, e: u128, m: u128, r: u128) -> u128 {
-    if e == 0 {
-        r
-    } else {
-        modpow0(b * b % m, e >> 1, m, if e & 1 == 1 { r * b % m } else { r })
-    }
 }
