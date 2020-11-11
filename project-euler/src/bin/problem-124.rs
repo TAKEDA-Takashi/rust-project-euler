50a7d38307ffa540cd9a53dbffcc4a743535bdf4
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-11-11 12:17:52 +0900

    feat: answer problem-124

---
+++ project-euler/src/bin/problem-124.rs
@@ -0,0 +1,19 @@
+//! http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%20124
+//! Ordered radicals
+
+use euler_lib::Prime;
+use itertools::Itertools;
+
+fn main() {
+    let ubound = 100_000;
+
+    let mut prime = Prime::<usize>::new();
+
+    println!(
+        "{:?}",
+        (1..=ubound)
+            .map(|n| (prime.factorization(&n).iter().dedup().product::<usize>(), n))
+            .sorted()
+            .nth(10000 - 1) // 0-origin
+    );
+}
