8ca640c51ec3c88f2b5580080e141a894a21614d
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2020-11-18 09:47:20 +0900

    refactor: problem-132

--- project-euler/src/bin/problem-132.rs
+++ project-euler/src/bin/problem-132.rs
@@ -6,55 +6,22 @@
 //!
 //! R(109) の最初の40個の素因数の和を求めよ.
 
-// R(n) は nの約数をm とすると R(m) で割り切れる
+// R(n) = (10**n - 1) / 9
+// (10**n - 1) / 9 == 0 mod p
+// 10**n - 1 == 0 mod p
+// 10**n == 1 mod p
 
-use euler_lib::{get_divisors, Prime};
-use itertools::repeat_n;
-use num::{BigUint, Zero};
-use std::collections::{BTreeSet, HashMap};
+use euler_lib::{modpow, Prime};
 
 fn main() {
-    let prime = Prime::<u128>::new();
+    let exp = 10_u32.pow(9);
 
-    let divisors = vec![
-        2, 4, 5, 8, 10, 16, 20, 25, 32, 40, 50, 64, 80, 100, 125, 128, 160, 200, 250, 256, 320,
-        400, 500, 512, 625, 640, 800, 1000, 1250, 1280, 1600, 2000, 2500, 2560, 3125, 3200, 4000,
-        5000, 6250, 6400, 8000, 10000, 12500, 12800, 15625, 16000, 20000, 25000, 31250, 32000,
-        40000, 50000, 62500,
-    ];
-
-    let mut rp = BTreeSet::new();
-    let mut table = HashMap::new();
-
-    for d in divisors {
-        let last = *get_divisors(&d).last().unwrap();
-        let mut r = repeat_n("1", d)
-            .collect::<String>()
-            .parse::<BigUint>()
-            .unwrap();
-
-        if table.contains_key(&last) {
-            r /= repeat_n("1", last)
-                .collect::<String>()
-                .parse::<BigUint>()
-                .unwrap();
-        }
-
-        table.insert(d, r);
-    }
-
-    for p in prime.iter() {
-        for (_, v) in &mut table {
-            if (v.clone() % p).is_zero() {
-                rp.insert(p);
-                *v = v.clone() / p;
-            }
-        }
-
-        if rp.len() == 40 {
-            break;
-        }
-    }
-
-    println!("{}", rp.iter().sum::<BigUint>());
+    println!(
+        "{}",
+        Prime::<u128>::new()
+            .iter()
+            .filter(|&p| p != 3 && modpow(10, exp, p) == 1)
+            .take(40)
+            .sum::<u128>()
+    );
 }
